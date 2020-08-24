8b3233bca543520df8be381b149ca0c393bef5a4
mpcarolin <mpcarolin.dev@gmail.com>
2020-08-24 13:48:47 -0700

    fixed build that was including the retheme in the bundle

--- configs/rollup.config.js
+++ configs/rollup.config.js
@@ -28,6 +28,9 @@ const shared = {
     'react-native',
     '@svkeg/jsutils',
     '@svkeg/re-theme',
+    // "./node_modules/@svkeg/re-theme/build/esm/reTheme.js",
+    // "./node_modules/@svkeg/re-theme/build/esm/reTheme.native.js",
+    '/Users/michael.carolin/keg/keg-components/node_modules/@svkeg/re-theme/build/esm/reTheme.js',
     'prop-types',
     'expo-fonts'
   ],
